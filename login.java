3f6c3766773ea95aa57257baa76ec6532a68871b
mzaenuddin <zaenuddinbl@gmail.com>
2020-07-11 15:40:29 +0700

    Nomor 2b Essay Question Topic

---
+++ login.java
@@ -0,0 +1,16 @@
+package package1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.firefox.firefoxDriver;
+
+public class LoginBukalapak{
+    public static void main(string[] args)
+    {
+        WebDriver driver = new firefoxFriver();
+        driver.get("https//bukalapak.com");
+        driver.findElement(By.id("user_session_username")).sendKeys("rmlatansa@gmail.com");
+        driver.findElement(By.id("user_session_password")).sendKeys("PASSWORD");
+        driver.findElement(By.id("commit")).click();
+    }
+}
